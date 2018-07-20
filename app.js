2a15ba7d4708a3768420bce615bd11e26a366cb5
MarkNewcomb1 <wlmmn@hotmail.com>
2018-07-20 12:19:56 -0600

    troubleshooting node .then

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
 const express = require("express");
+const app = express();
 const cors = require("cors");
-let csvToJson = require('convert-csv-to-json');
-var cohorts = csvToJson.fieldDelimiter(',').getJsonFromCsv("cohorts.csv");
-console.log(cohorts);
+const morgan = require('morgan');
+// let csvToJson = require('convert-csv-to-json');
+const bodyParser = require('body-parser');
+// var cohorts = csvToJson.fieldDelimiter(',').getJsonFromCsv("cohorts.csv");
+
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cors());
+
+var cohorts = [{
+    id: 1,
+    cohortName: "17-01-WD-DP",
+    cohortCode: "g100",
+    numberOfStudents: 28
+},{
+    id: 2,
+    cohortName: "17-01-DS-GT",
+    cohortCode: "g105",
+    numberOfStudents: 24
+},{
+    id: 3,
+    cohortName: "17-02-WD-PX",
+    cohortCode: "g109",
+    numberOfStudents: 30
+},{
+    id: 4,
+    cohortName: "17-03-WD-BD",
+    cohortCode: "g110",
+    numberOfStudents: 29
+}];
+
 function findById(data, id){
     for (let i = 0; i < data.length; i++){
         if (data[i].id === id){
@@ -11,17 +44,25 @@ function findById(data, id){
     }
     return null;
 }
+app.get("/home",(request, response) => {
+    // console.log("REQUEST: " + request);
+    // console.log("RESPONSE: " + response);
+    
+    response.json(cohorts)
+    .then(data => {
+        console.log("DATA: " + data);
+        
+    })
+})
 
-const app = express();
-app.use(cors());
-
-app.get("/", function (request, response) {
-    response.json({cohorts});
-});
 
 app.get("/:id", function (request, response) {
-    var record = findById(cohorts, request.params.ID);
-    console.log("COHORTS: " + cohorts, "request.params.ID " + request.params.ID);
+    // console.log("REQUEST: " + request.params);
+    
+    var record = findById(cohorts, request.params.id);
+    // console.log("RECORD: " + record);
+    
+    // console.log("COHORTS: " + cohorts, "request.params.ID " + request.params.ID);
     
     if (!record){
         response.status = 404;
@@ -32,7 +73,23 @@ app.get("/:id", function (request, response) {
         });
     }
 
-    response.json({cohorts: record});
+    //response.json({cohorts: record});
 });
+
+// catch 404 and forward to error handler
+// app.use((req, res, next) => {
+//     const err = new Error("Not Found");
+//     err.status = 404;
+//     next(err);
+// });
+
+// // error handler
+// app.use((err, req, res, next) => {
+//     res.status(err.status || 500);
+//     res.json({
+//     message: err.message,
+//     error: req.app.get("env") === "development" ? err.stack : {}
+//     });
+// });
 
 app.listen(9000);
